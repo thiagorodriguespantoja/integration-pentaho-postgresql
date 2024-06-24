2aee22af6a634122ccc8b09262d5a3c7c476c85b
Thiago Pantoja <thiago.pantoja@enkel.com.br>
2024-06-24 18:40:20 -0300

    📦 NEW: feat add configure-pentaho.sh

---
+++ pentaho/init-scripts/configure-pentaho.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Configuração do Pentaho
+echo "Configurando o Pentaho..."
+
+# Definir variáveis de ambiente
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PENTAHO_HOME=/opt/data-integration
+
+# Configurar permissões de execução para os scripts do Pentaho
+chmod +x $PENTAHO_HOME/*.sh
+
+# Exemplo de configuração adicional, se necessário
+# mv /path/to/some/config/file $PENTAHO_HOME/.kettle/
+
+# Mensagem de conclusão
+echo "Configuração do Pentaho concluída."
