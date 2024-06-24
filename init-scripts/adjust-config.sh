e67400fc6bad6865f3223b3ebd5b4a8ef8ab345e
Thiago Pantoja <thiago.pantoja@enkel.com.br>
2024-06-24 16:48:15 -0300

    🚀 RELEASE: feat update adjust-config (Script)

--- init-scripts/adjust-config.sh
+++ init-scripts/adjust-config.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 # Script para configurar o PostgreSQL
 
-# Verifica se o script está sendo executado pelo usuário postgres
-if [ "$EUID" -ne $(id -u postgres) ]; then
-  echo "Por favor, execute este script como o usuário 'postgres'."
-  exit 1
-fi
-
 # Ajusta o arquivo postgresql.conf para permitir conexões externas
 echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf
 
 # Ajusta o arquivo pg_hba.conf para permitir acesso a partir de qualquer IP
 echo "host all all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
 
-# Inicia o PostgreSQL
-pg_ctl -D /var/lib/postgresql/data start
+# Verifica se o PostgreSQL está em execução antes de tentar iniciar
+pg_isready -q
+if [ $? -ne 0 ]; then
+    pg_ctl -D /var/lib/postgresql/data_pg start
+fi
+
 
 
