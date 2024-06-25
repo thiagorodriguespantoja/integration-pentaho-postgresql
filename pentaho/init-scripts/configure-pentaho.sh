45145a77ed935d157f203556d8ab123958152347
Thiago Pantoja <thiago.pantoja@enkel.com.br>
2024-06-24 22:14:48 -0300

    🐛 FIX:  Atualizar Dockerfile e docker-compose.yml para expor portas necessárias e manter o container ativo"

--- pentaho/init-scripts/configure-pentaho.sh
+++ pentaho/init-scripts/configure-pentaho.sh
@@ -3,17 +3,6 @@
 # Configuração do Pentaho
 echo "Configurando o Pentaho..."
 
-# Verificar se os diretórios existem
-if [ ! -d "/usr/lib/jvm/java-17-openjdk-amd64" ]; then
-    echo "Diretório JAVA_HOME não encontrado. Certifique-se de que o Java está instalado."
-    exit 1
-fi
-
-if [ ! -d "/opt/data-integration" ]; then
-    echo "Diretório PENTAHO_HOME não encontrado. Certifique-se de que o Pentaho está instalado."
-    exit 1
-fi
-
 # Definir variáveis de ambiente
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PENTAHO_HOME=/opt/data-integration
@@ -23,5 +12,6 @@ chmod +x $PENTAHO_HOME/*.sh
 
 # Mensagem de conclusão
 echo "Configuração do Pentaho concluída."
+
 
 
